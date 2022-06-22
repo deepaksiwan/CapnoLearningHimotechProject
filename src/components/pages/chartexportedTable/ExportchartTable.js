ff446fc34e16f25777767f2b8209d70120926445
deepak kumar <deepak.kumar@himotechglobal.com>
2022-06-22 18:47:15 +0530

    update code

--- src/components/pages/chartexportedTable/ExportchartTable.js
+++ src/components/pages/chartexportedTable/ExportchartTable.js
@@ -10,7 +10,7 @@ import { csv } from 'd3';
 import { jsPDF } from "jspdf";
 import ChartExportedHeader from '../../component/ChartExportedHeader';
 import ExportChart from './ChartExported';
-// import folderimage from '../../images/choosefile.png'
+import folderimage from '../../images/choosefile.png'
 import { red } from '@material-ui/core/colors';
 
 
@@ -107,24 +107,21 @@ const ChartTable = (props) => {
     }
     const SaveImage = (_imageArray) => {
         const doc = new jsPDF();
-        //const pages = doc.internal.getNumberOfPages();
-        //console.log("pages", pages)
-        console.log("doc",doc)
-        console.log("pdf result", doc.getNumberOfPages())
         for (let pageNumber = 1; pageNumber <= doc.getNumberOfPages(); pageNumber++) {
-            //console.log("pagenumber",pageNumber)
             doc.setPage(pageNumber)
             doc.setTextColor(2, 4, 7);
             doc.text('Capnolearning Report', 10, 10,
-            { styles: { fontSize: 20, fontWeight: 'bold' } })
+                { styles: { fontSize: 20, fontWeight: 'bold' } })
             doc.setDrawColor(0, 0, 0);
             doc.line(10, 15, 600, 15);
             doc.setFontSize(10);
             doc.setFont(undefined, 'bold');
-            
             doc.text("Session Date:", 10, 25)
             doc.text("Client:", 10, 30);
             doc.text("Trainer:", 10, 35);
+            console.log("lenght of iamge", _imageArray.length - 4)
+            let ev = 0;
+
             _imageArray.length > 0 && _imageArray.map((v, i) => {
                 let reader = new FileReader();
                 console.log("result of v", v)
@@ -133,22 +130,26 @@ const ChartTable = (props) => {
                     reader.readAsDataURL(v);
                     reader.onload = event => {
                         let dataimage = event.target.result;
-                        //doc.addImage(dataimage, 7, 50, 200, 90)
-                        doc.addImage(dataimage, 7, 100*(1+i), 200, 100);
-                
-                        // doc.addImage(dataimage, 7, 60*(4+i), 200, 90);
-                        // doc.addImage(dataimage, 7, 60*(5+i), 200, 90);
-                        doc.setPage(i + 1);
-                      
+                        console.log("dataimage", 
+                        dataimage)
+                        doc.addImage(dataimage, 7, 50 + (ev * 110), 200, 90);
+                        //doc.addImage(dataimage, 3, 0 + (ev * 0), pageWidth, pageHeight);
+                        ev++;
+                        if (ev == 2) {
+                            ev = 0;
+                            doc.addPage();
+                        }
+
                     };
+
                 }
             })
-           
+
         }
         setTimeout(() => {
             doc.save("Image.pdf");
-           
-        }, _imageArray.length  * 3000);
+
+        }, _imageArray.length * 2000);
     }
     return (
         <div>
@@ -169,50 +170,52 @@ const ChartTable = (props) => {
                             })
                         }
                         {
-                            !fileSelected &&
-                            <div className="wrp-chart-loader">
-                                <div className="uploadfile" >
-                                    <div className='uploadheader'>
-                                        <p style={{ color: "#800080", marginLeft: "35rem", fontSize: "17px" }} >Please choose folder to visualise signal data in the selected report template. </p>
-                                    </div>
-                                    <div className='files'>
-                                        <label id="Choosefolder">
-                                            <button className="buttonstyle d-flex" style={{ border: "1 px solid", background: "white", borderRadius: "5px" }}>
-                                                {/* <label><img style={{ marginRight: "10px", marginTop: "3px" }} src={folderimage}></img></label> */}
-                                                <h6 style={{ color: "#800080", marginTop: "5px" }}>Choose Folder</h6>
-                                            </button>
-                                        </label>
-                                        <input id="ChooseFolder" multiple type="file" style={{ opacity: "0", position: "obsolute", marginLeft: "-190px", borderRadius: 'none' }} onChange={fileupload} webkitdirectory="true"
-                                        />
-                                    </div>
-                                    <div className="" style={{ marginLeft: "34rem", color: "#800080" }}>
-                                        <a onClick={toggleHelpModal}>
+                           !fileSelected &&
+                           <div className='col-md-12'>
+                           <div className="bag-1">
+                               <div className="uploadfile ">
+                                   <div className=' content1'>
+                                       <p className='bag-10'>Please choose folder to visualise signal data in the selected report template. </p>
+                                   </div>
+                                   <div className='content2 '>
+                                       <div className='bag-2'>
+                                           <button className="buttonstyle" >
+                                               <label><img style={{ marginRight: "10px", marginTop: "3px" }} src={folderimage}></img></label>
+                                               <h6 style={{ color: "#800080", marginTop: "5px", display: "inline-block" }}>Choose Folder</h6>
+                                           </button>
+                                           <input id="ChooseFolder" multiple type="file" style={{ opacity: "0", position: "obsolute", marginLeft: "-190px", borderRadius: 'none' }} onChange={fileupload} webkitdirectory="true"
+                                       />
+                                       </div>
 
-                                            <span>Need help</span>
-                                            <span><i class="fa fa-question-circle need-" aria-hidden="true"></i></span>
+                                      
+                                   </div>
+                                   <div className=" content3 " >
+                                       <a href='#' onClick={toggleHelpModal}>
+                                          <p className='bag-10'>Need Help</p>
+                                           <span><i class="fa fa-question-circle need- bag-200" aria-hidden="true"></i></span>
 
-                                        </a>
+                                       </a>
+                                   </div>
 
-                                    </div>
+                               </div>
+                               <Modal isOpen={HelpModal} toggle={toggleHelpModal} className="modal-box-wrp" centered={true}>
+                                   <ModalHeader toggle={toggleHelpModal}><span className="ml-1 roititle modal-head">You Need to Help.... </span></ModalHeader>
+                                   <ModalBody>
+                                       <ul className="range-list">
+                                           <h5>
+                                               hdjshdkdsdkshd
+                                               jdfsdkfs
+                                               dsjsdjfa-spinjdfsdjfd
+                                               kfkfs
+                                           </h5>
+                                       </ul>
+                                   </ModalBody>
 
-                                </div>
-                                <Modal isOpen={HelpModal} toggle={toggleHelpModal} className="modal-box-wrp" centered={true}>
-                                    <ModalHeader toggle={toggleHelpModal}><span className="ml-1 roititle modal-head">You Need to Help.... </span></ModalHeader>
-                                    <ModalBody>
-                                        <ul className="range-list">
-                                            <h5>
-                                                hdjshdkdsdkshd
-                                                jdfsdkfs
-                                                dsjsdjfa-spinjdfsdjfd
-                                                kfkfs
-                                            </h5>
-                                        </ul>
-                                    </ModalBody>
-
-                                </Modal>
+                               </Modal>
 
 
-                            </div>
+                           </div>
+                           </div>
 
                         }
                     </div>
