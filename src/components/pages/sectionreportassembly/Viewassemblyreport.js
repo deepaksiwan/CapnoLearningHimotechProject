9cbd894e9f256694fd0149947d5de4c7d302ae25
md nasim <mdnasim4234@gmail.com>
2022-07-15 12:03:39 +0530

    latest code

--- src/components/pages/sectionreportassembly/Viewassemblyreport.js
+++ src/components/pages/sectionreportassembly/Viewassemblyreport.js
@@ -1,5 +1,5 @@
-import React, {useEffect,useState} from "react";
-import {Link,useParams, Router} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useParams, Router } from 'react-router-dom';
 import { Row, Col, Container, Button, ModalHeader, ModalFooter, Modal, ModalBody } from "reactstrap";
 import download from 'downloadjs';
 import Sidebar from '../../component/Sidebar';
@@ -21,19 +21,23 @@ const Viewassemblyreport = () => {
     const [itemId, setItemId] = useState(null);
     const [deleteModal, setdeleteModal] = useState(false);
     const deleteToggleModal = () => setdeleteModal(!deleteModal);
+    const [downloaderModal, setDownloaderModal] = useState(false);
+    const DownloaderToggleModal = () => setDownloaderModal(!downloaderModal);
+    const [openModal, setOpenModal] = useState(false);
+    const openToggleModal = () => setOpenModal(!openModal);
 
     useEffect(() => {
         getassebllyList();
 
 
-        const interval = setInterval(()=>{
+        const interval = setInterval(() => {
             getassebllyList();
-        },3000);
-        return()=> clearInterval(interval);
+        }, 3000);
+        return () => clearInterval(interval);
 
     }, []);
 
-    
+
     const getassebllyList = () => {
 
 
@@ -53,9 +57,9 @@ const Viewassemblyreport = () => {
                     console.warn("result", resp);
                     const clientName = resp.firstname + resp.lastname
                     const sessionDate = resp.sessionDate
-                  
-                 console.log(clientName)
-                    
+
+                    console.log(clientName)
+
                     let _temp = [];
                     console.log(_temp)
                     resp.data.map((v, i) => {
@@ -63,9 +67,9 @@ const Viewassemblyreport = () => {
                             reportTitle: v.name,
                             Client: clientName,
                             Session: sessionDate,
-                            dateCreated: new Date(v.created_at*1000).toLocaleString(),
-                            actions: <p><a href="#" onClick={()=> viewpdf(v.id)} className="downloadimg" ><img src={preveiw} /></a><a href={"/view/edit/assemblyreport/" + v.id} className="downloadimg" ><img src={edit} /></a> <a href='#' onClick={()=> downloadpdf(v.id)} className="downloadimg"><img src={downloads} /></a> <a className="downloadimg" onClick={() => openItemPopUp(v.id)} ><img src={Delete} /></a></p>
-                            
+                            dateCreated: new Date(v.created_at * 1000).toLocaleString(),
+                            actions: <p><a href="#" onClick={() => {viewpdf(v.id); openToggleModal()}} className="downloadimg" ><img src={preveiw} /></a><a href={"/view/edit/assemblyreport/" + v.id} className="downloadimg" ><img src={edit} /></a> <a href='#' onClick={() => {downloadpdf(v.id); DownloaderToggleModal()}} className="downloadimg"><img src={downloads} /></a> <a className="downloadimg" onClick={() => openItemPopUp(v.id)} ><img src={Delete} /></a></p>
+
                         })
                     })
                     setData(_temp);
@@ -86,12 +90,12 @@ const Viewassemblyreport = () => {
 
     }
 
-   
-   
+
+
 
 
     const downloadpdf = (id) => {
-      
+
         fetch(API_URL + "/get/full/screenshort/" + id + "/" + Clientid,
             {
                 method: 'GET',
@@ -101,23 +105,25 @@ const Viewassemblyreport = () => {
                 },
             }
         ).then(res => res.blob())
-            .then(response => {
+        
+            .then(res => {
                 //Create a Blob from the PDF Stream
-                console.log(response);
-              
-                const file = new Blob([response], {
+                
+                const file = new Blob([res], {
                     type: "application/pdf"
                 });
+               setDownloaderModal(!downloaderModal)
                 //Build a URL from the file
                 const fileURL = URL.createObjectURL(file);
-               
                 download(fileURL);
 
             })
+           
+           
     }
 
     const viewpdf = (id) => {
-      
+
         fetch(API_URL + "/get/full/screenshort/" + id + "/" + Clientid,
             {
                 method: 'GET',
@@ -130,7 +136,7 @@ const Viewassemblyreport = () => {
             .then(response => {
                 //Create a Blob from the PDF Stream
                 console.log(response);
-              
+
                 const file = new Blob([response], {
                     type: "application/pdf"
                 });
@@ -138,7 +144,7 @@ const Viewassemblyreport = () => {
                 const fileURL = URL.createObjectURL(file);
                 //Open the URL on new Window
                 window.open(fileURL);
-               
+
 
             })
     }
@@ -177,8 +183,8 @@ const Viewassemblyreport = () => {
         })
 
     }
-   
-    const columns =[
+
+    const columns = [
         {
             title: "Client", field: "Client"
         },
@@ -197,46 +203,84 @@ const Viewassemblyreport = () => {
     ]
 
 
-    return(
+    return (
         <div className="">
             <Header />
-             <div className="wrp-dashbord">
+            <div className="wrp-dashbord">
                 <div className="sidebar-section">
-                <Sidebar />
-               </div>
-               <div className="right-section">
-                <div className="head-demoreport">
-                    <h3>Session Assembly Reports</h3>
-
+                    <Sidebar />
                 </div>
-                <div className="wrp-bankform">
-                    <div style={{ maxWidth: '100%' }}>
-                        <MaterialTable
-                        columns={columns}
-                        data={data}
-                        title=""
-                        />
-                        
+                <div className="right-section">
+                    <div className="head-demoreport">
+                        <h3>Session Assembly Reports</h3>
+
+                    </div>
+                    <div className="wrp-bankform">
+                        <div style={{ maxWidth: '100%' }}>
+                            <MaterialTable
+                                columns={columns}
+                                data={data}
+                                title=""
+                            />
+
+                        </div>
                     </div>
                 </div>
-               </div>
-             </div>
-            
-             <Modal isOpen={deleteModal} toggle={deleteToggleModal} className="connect-box" centered={true}>
-                        <ModalHeader toggle={deleteToggleModal}><span className="ml-1 roititle font-weight-bold">Delete</span></ModalHeader>
-                        <ModalBody>
-                            <div className="modal-p">
-                                <div className="right-circle cancel-circle"><img src={closeicon} /></div>
-                                <h4>Are You Sure?</h4>
-                                <p>Do you really want to delete this record?</p>
-                                <div className="wrp-delete-btn">
-                                    <div className="cancel-btn1" ><a onClick={deleteToggleModal}>Cancel</a></div>
-                                    <div className="delete-btn1"><a onClick={deleteClient}>Delete</a></div>
-                                </div>
-                            </div>
-                        </ModalBody>
+            </div>
 
-                    </Modal>
+            <Modal isOpen={deleteModal} toggle={deleteToggleModal} className="connect-box" centered={true}>
+                <ModalHeader toggle={deleteToggleModal}><span className="ml-1 roititle font-weight-bold">Delete</span></ModalHeader>
+                <ModalBody>
+                    <div className="modal-p">
+                        <div className="right-circle cancel-circle"><img src={closeicon} /></div>
+                        <h4>Are You Sure?</h4>
+                        <p>Do you really want to delete this record?</p>
+                        <div className="wrp-delete-btn">
+                            <div className="cancel-btn1" ><a onClick={deleteToggleModal}>Cancel</a></div>
+                            <div className="delete-btn1"><a onClick={deleteClient}>Delete</a></div>
+                        </div>
+                    </div>
+                </ModalBody>
+
+            </Modal>
+
+
+            <Modal isOpen={downloaderModal} toggle={DownloaderToggleModal} className="connect-box" centered={true}>
+                <ModalHeader toggle={DownloaderToggleModal}><span className="ml-1 roititle font-weight-bold">Downloading</span></ModalHeader>
+                <ModalBody>
+                    <div className="modal-p">
+                        <div class="loading2">
+                            <div class="dot">L</div>
+                            <div class="dot">O</div>
+                            <div class="dot">A</div>
+                            <div class="dot">D</div>
+                            <div class="dot">I</div>
+                            <div class="dot">N</div>
+                            <div class="dot">G</div>
+                            <span class="text">Please Wait...</span>
+                        </div>
+                    </div>
+                </ModalBody>
+
+            </Modal>
+            <Modal isOpen={openModal} toggle={openToggleModal} className="connect-box" centered={true}>
+                <ModalHeader toggle={openToggleModal}><span className="ml-1 roititle font-weight-bold">Opening</span></ModalHeader>
+                <ModalBody>
+                    <div className="modal-p">
+                        <div class="loading2">
+                            <div class="dot">O</div>
+                            <div class="dot">P</div>
+                            <div class="dot">E</div>
+                            <div class="dot">N</div>
+                            <div class="dot">I</div>
+                            <div class="dot">N</div>
+                            <div class="dot">G</div>
+                            <span class="text">Please Wait...</span>
+                        </div>
+                    </div>
+                </ModalBody>
+
+            </Modal>
         </div>
     )
 }
