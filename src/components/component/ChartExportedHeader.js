b44d0d2721b5234bf716811732e25024425f8bcf
md nasim <mdnasim4234@gmail.com>
2022-12-08 18:31:58 +0530

    latest code

--- src/components/component/ChartExportedHeader.js
+++ src/components/component/ChartExportedHeader.js
@@ -16,18 +16,18 @@ const ChartExportedHeader = (props) => {
     // const [sessionDate, setsessionDate] = useState([]);
     const [clientName, setClientName] = useState([]);
     const [trainerName, setTrainerName] = useState([]);
-    const sessioninfo = props.sessioninfo ; 
-    const setrequestProcessingModal  = props.setrequestProcessingModal ; 
-    const setrequestProcesedModal  = props.setrequestProcesedModal ; 
-    const showHeader = props.showHeader ; 
+    const sessioninfo = props.sessioninfo;
+    const setrequestProcessingModal = props.setrequestProcessingModal;
+    const setrequestProcesedModal = props.setrequestProcesedModal;
+    const showHeader = props.showHeader;
     const reportconfig = useRef();
-    const exportExcel = props.exportExcel ; 
-    const graphs = props.graphs ;
+    const exportExcel = props.exportExcel;
+    const graphs = props.graphs;
     const ExcelFile = ReactExport.ExcelFile;
     const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
     const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
-    const setShowSignalStat = props.setShowSignalStat ;
-    const showSignalStat = props.showSignalStat ;
+    const setShowSignalStat = props.setShowSignalStat;
+    const showSignalStat = props.showSignalStat;
 
     const { config, session, record } = useParams();
     // const [selectfolder, setselectfolder] = useState(false)
@@ -35,8 +35,8 @@ const ChartExportedHeader = (props) => {
     //  const [selectedfiles, setselectedfiles] = useState([])
     //const [fileSelected, setfileSelected] = useState([])
     const fileupload = props.fileupload;
-    const signalStat = props.signalStat ; 
-    const [liveNotes,setLiveNotes] = useState(props.sessioninfo.session  ? props.sessioninfo.session.notes : []) ; 
+    const signalStat = props.signalStat;
+    const [liveNotes, setLiveNotes] = useState(props.sessioninfo.session ? props.sessioninfo.session.notes : []);
 
     const [notesModal, setNotesModal] = useState(false);
     const notesModalToggle = () => setNotesModal(!notesModal);
@@ -45,7 +45,7 @@ const ChartExportedHeader = (props) => {
 
     useEffect(() => {
         // console.log("mydata" , signalStat);
-    },[signalStat])
+    }, [signalStat])
     const downloadImage = props.downloadImage;
     useEffect(() => {
         // Report();
@@ -54,7 +54,7 @@ const ChartExportedHeader = (props) => {
         // SaveImage();
     }, []);
     const getRcord = () => {
-        fetch(API_URL+"/session/record?session_id=" + sessionid,
+        fetch(API_URL + "/session/record?session_id=" + sessionid,
             {
                 method: 'GET',
                 headers: {
@@ -135,7 +135,7 @@ const ChartExportedHeader = (props) => {
     }
 
     const viewManual = () => {
-        window.open('/manualpdf/OperatingManualP6.0-November25-2022.pdf/manualpdf/OperatingManualP6.0-November25-2022.pdf','Manual','height=768,width=500');
+        window.open('/manualpdf/OperatingManualP6.0-November25-2022.pdf/manualpdf/OperatingManualP6.0-November25-2022.pdf', 'Manual', 'height=768,width=500');
     }
     // const choosefolder = (event) => {
     //     setselectfolder(true)
@@ -180,37 +180,37 @@ const ChartExportedHeader = (props) => {
             // doc.text("Trainer:", 10, 35);
             doc.text("Live Session Notes:", 10, 45);
             doc.setFont(undefined, 'normal');
-            
+
             var splitTitle = doc.splitTextToSize(document.getElementById("liveNotes").innerHTML, 270);
             var pageHeight = doc.internal.pageSize.height;
-           
+
             var y = 50;
-            for (var i = 0; i < splitTitle.length; i++) {                
+            for (var i = 0; i < splitTitle.length; i++) {
                 if (y > 280) {
                     y = 10;
                     doc.addPage();
                 }
                 // // console.log("line" , splitTitle[i])
-                doc.text(splitTitle[i].replace(/(<([^>]+)>)/gi, "") , 10, y);
+                doc.text(splitTitle[i].replace(/(<([^>]+)>)/gi, ""), 10, y);
                 y = y + 3;
             }
         }
- 
-        doc.save("Live Session Notes - "+sessioninfo[0].name + "-" + sessioninfo[0].client_firstname+ " " + sessioninfo[0].client_lastname + ".pdf");
+
+        doc.save("Live Session Notes - " + sessioninfo[0].name + "-" + sessioninfo[0].client_firstname + " " + sessioninfo[0].client_lastname + ".pdf");
         setrequestProcessingModal(false);
         setrequestProcesedModal(true);
-          
+
     }
-    
+
     const saveScreenshot = () => {
         // // console.log(sessioninfo);
-        setrequestProcessingModal(true) ; 
+        setrequestProcessingModal(true);
         html2canvas(document.getElementById("exported-chart")).then(function (canvas) {
 
-          
+
             const doc = new jsPDF();
             let dataimg = canvas.toDataURL('image/png')
-          
+
 
             for (let pageNumber = 1; pageNumber <= doc.getNumberOfPages(); pageNumber++) {
                 doc.setPage(pageNumber)
@@ -232,15 +232,15 @@ const ChartExportedHeader = (props) => {
                 // doc.setFont(undefined, 'bold')
                 doc.addImage(dataimg, 5, 45, 200, 110);
             }
-    
-           setTimeout(() => {
-            doc.save("PDF Report - "+sessioninfo.session.date   + ".pdf");
-            setrequestProcessingModal(false) ; 
-            // () ;
-            setrequestProcesedModal(true) ;
-            
-           }, 5000);
-            
+
+            setTimeout(() => {
+                doc.save("PDF Report - " + sessioninfo.session.date + ".pdf");
+                setrequestProcessingModal(false);
+                // () ;
+                setrequestProcesedModal(true);
+
+            }, 5000);
+
 
 
         });
@@ -250,7 +250,7 @@ const ChartExportedHeader = (props) => {
 
     return (
         <div className="bg-c-header">
-                                <ReactTooltip />
+            <ReactTooltip />
 
             <div className="wrp-chart-header">
                 <div className="chart-header-c1">
@@ -258,37 +258,37 @@ const ChartExportedHeader = (props) => {
                         <div className="action-opt" >
                             <p>Actions Options</p>
                             <ul className='action-list2 whiteicon' >
-                            {
-                               showHeader && sessioninfo.session && 
-                                   
-                                <li>
-                                <ExcelFile filename={"Statistics - "+sessioninfo.session.date} element={<a href="javascript:void" data-title="Session date" onClick={exportExcel} data-tip="Export session data as Excel Sheet."   ><i class="fa fa-upload whiteicon" aria-hidden="true"></i></a>}>
-                               
-                                    {
-                                       graphs.map((v,i) => {
-                                        //       // console.log("excel data "+v.signal_name , signalStat[v.signal_name] );
-                                        // if( v.signal_name != "pco2wave"  && v.signal_name != "pco2b2b"  && v.signal_name != "capin" && v.signal_name != "b2b2hr" && v.signal_name != "b2brsa" ){
-                                          return (
+                                {
+                                    showHeader && sessioninfo.session &&
 
-                                    <ExcelSheet data={signalStat[v.signal_name] ? signalStat[v.signal_name] : [] } name={v.signal_name}>
-                                        <ExcelColumn label="X" value="x"/>
-                                        <ExcelColumn label="Mean" value="mean"/>
-                                        <ExcelColumn label="Median" value="mean"/>
-                                        <ExcelColumn label="Standard Deviation" value="sd"/>
-                                    </ExcelSheet>                                          )
-                                        // }
-                                        
-                                      })
-                                    }
-              
-                
-            </ExcelFile>
-            </li>
- 
-}
+                                    <li>
+                                        <ExcelFile filename={"Statistics - " + sessioninfo.session.date} element={<a href="javascript:void" data-title="Session date" onClick={exportExcel} data-tip="Export session data as Excel Sheet."   ><i class="fa fa-upload whiteicon" aria-hidden="true"></i></a>}>
 
-                                  {/* <li><a href="#"><i class="fa fa-sticky-note" aria-hidden="true"></i></a></li> */}
-                                <li data-tip="Save Report as PDF"><a href="javascript:void"   onClick={saveScreenshot}><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a></li>
+                                            {
+                                                graphs.map((v, i) => {
+                                                    //       // console.log("excel data "+v.signal_name , signalStat[v.signal_name] );
+                                                    // if( v.signal_name != "pco2wave"  && v.signal_name != "pco2b2b"  && v.signal_name != "capin" && v.signal_name != "b2b2hr" && v.signal_name != "b2brsa" ){
+                                                    return (
+
+                                                        <ExcelSheet data={signalStat[v.signal_name] ? signalStat[v.signal_name] : []} name={v.signal_name}>
+                                                            <ExcelColumn label="X" value="x" />
+                                                            <ExcelColumn label="Mean" value="mean" />
+                                                            <ExcelColumn label="Median" value="mean" />
+                                                            <ExcelColumn label="Standard Deviation" value="sd" />
+                                                        </ExcelSheet>)
+                                                    // }
+
+                                                })
+                                            }
+
+
+                                        </ExcelFile>
+                                    </li>
+
+                                }
+
+                                {/* <li><a href="#"><i class="fa fa-sticky-note" aria-hidden="true"></i></a></li> */}
+                                <li data-tip="Save Report as PDF"><a href="javascript:void" onClick={saveScreenshot}><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a></li>
                                 {/* <li><a href="#" for="file"><i class="fa fa-download" aria-hidden="true"></i></a>
                                 </li> */}
                                 <li data-tip="Load Different Client/Session">
@@ -304,13 +304,13 @@ const ChartExportedHeader = (props) => {
                         <div className="view-opt" >
                             <p>Viewing Options</p>
                             <ul className='action-list3 whiteicon'>
-                            
+
                                 <li data-tip="View live session notes"><a onClick={notesModalToggle} href="javascript:void" ><i class="fa fa-file" aria-hidden="true"></i></a></li>
                                 <li data-tip="View session images PDF"><a href="javascript:void" onClick={downloadImage}><i class="fa fa-image" aria-hidden="true"></i></a></li>
                                 {/* <li><a href="#"><i class="fa fa-video-camera" aria-hidden="true"></i></a></li>
                                 <li><a href="#"><i class="fa fa-step-backward" aria-hidden="true"></i></a></li> */}
                                 <li data-tip="Toogle signals satistics"><a href="javascript:void" onClick={() => setShowSignalStat(!showSignalStat)}><i class="fa fa-table"></i></a></li>
-                                <li data-tip="View help documenyt"><a href="javascript:void"  onClick={viewManual}><i class="fa fa-question-circle" aria-hidden="true"></i></a></li>
+                                <li data-tip="View help documenyt"><a href="javascript:void" onClick={viewManual}><i class="fa fa-question-circle" aria-hidden="true"></i></a></li>
                                 {/* <li><a href="javascript:void"><i class="fa fa-clock-o" aria-hidden="true"></i></a></li> */}
                             </ul>
                         </div>
@@ -318,31 +318,31 @@ const ChartExportedHeader = (props) => {
                 </div>
                 <div className="chart-header-c3">
                     <ul className="username-list">
-                    <li data-tip="Name of client">
-                        {
-                        sessioninfo.session &&
- 
+                        <li data-tip="Name of client">
+                            {
+                                sessioninfo.session &&
+
                                 <a href="javascript:void"><i class="fa fa-user" aria-hidden="true"></i>{sessioninfo.session.client}</a>
-                          
-                        }
-                    
+
+                            }
+
                         </li>
                         <li data-tip="Name of trainer">
-                        { 
-                         sessioninfo.session &&
-                
+                            {
+                                sessioninfo.session &&
+
                                 <a href="javascript:void"><i class="fa fa-user-md" aria-hidden="true"></i> {sessioninfo.session.trainer} </a>
-                         
-                        }
-                  
+
+                            }
+
                         </li>
-                     <li data-tip="Session date">
-                        {
-                        sessioninfo.session &&    
-                               <a href="javascript:void"><i class="fa fa-calendar" aria-hidden="true"></i> {sessioninfo.session.date}</a>
-                         
-                        }
-                   
+                        <li data-tip="Session date">
+                            {
+                                sessioninfo.session &&
+                                <a href="javascript:void"><i class="fa fa-calendar" aria-hidden="true"></i> {sessioninfo.session.date}</a>
+
+                            }
+
                         </li>
                     </ul>
                 </div>
@@ -358,26 +358,26 @@ const ChartExportedHeader = (props) => {
             </div>
 
             <Modal isOpen={notesModal} toggle={notesModalToggle} className="modal-box-wrp" centered={true}>
-                        <ModalHeader toggle={notesModalToggle}><span className="ml-1 roititle modal-head"> Live Session Notes</span></ModalHeader>
-                        <ModalBody>
-                           <p id="liveNotes">{liveNotes && liveNotes.length > 0 ?
-                               liveNotes.map((v,i) => {
-                                   return(
-                                    <p dangerouslySetInnerHTML={{__html: v.sessiondata}}></p>
-                                   )
-                               })
-                            : "No notes available."}</p>
-                            
-                        <div className='d-flex justify-content-around mt-3'>
-                            <button className='lightbtn w-100'  onClick={notesModalToggle} >Cancel</button>
-                            {
-                               liveNotes && liveNotes.length > 0 &&
-                            <button className='darktbtn w-100 ml-1'  onClick={downloadNotesPDF} >Download PDF</button>
-                            }
-                        </div>
-                        </ModalBody>
+                <ModalHeader toggle={notesModalToggle}><span className="ml-1 roititle modal-head"> Live Session Notes</span></ModalHeader>
+                <ModalBody>
+                    <p id="liveNotes">{liveNotes && liveNotes.length > 0 ?
+                        liveNotes.map((v, i) => {
+                            return (
+                                <p dangerouslySetInnerHTML={{ __html: v.sessiondata }}></p>
+                            )
+                        })
+                        : "No notes available."}</p>
 
-                    </Modal>
+                    <div className='d-flex justify-content-around mt-3'>
+                        <button className='lightbtn w-100' onClick={notesModalToggle} >Cancel</button>
+                        {
+                            liveNotes && liveNotes.length > 0 &&
+                            <button className='darktbtn w-100 ml-1' onClick={downloadNotesPDF} >Download PDF</button>
+                        }
+                    </div>
+                </ModalBody>
+
+            </Modal>
 
         </div>
     )
